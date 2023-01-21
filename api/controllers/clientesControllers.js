f100e9f681b5b6ea77548334d8a188e04fcd826d
bruno dias <diasbruno00@gmail.com>
2023-01-21 09:27:03 -0300

    adicionado tratamento de execoes

--- api/controllers/clientesControllers.js
+++ api/controllers/clientesControllers.js
@@ -4,21 +4,19 @@ const ClienteDAO = require("../dataBase/clienteDao");
 function salvarDadosClientes(req, res, next) {
 
   const cliente = new Cliente(req.body.nome, req.body.idade, req.body.email);
-
   const clienteDao = new ClienteDAO();
 
   if (cliente.erros.length == 0) {
-
-    clienteDao.inserirClientesDB(cliente); // inserir cliente no banco de dados
-    req.flash("sucesso",`${cliente.nome} cadastrado com sucsso`)
-    res.redirect("/cadastro/cliente")
+    clienteDao.inserirClientesDB(cliente); 
+    req.flash("sucesso", `${cliente.nome} cadastrado com sucsso`);
+    res.redirect("/cadastro/cliente");
   } else {
-    req.flash("erro","verifique se os dados inseridos estao corretos")
-    res.redirect("/cadastro/cliente")
+    req.flash("erro", "verifique se os dados inseridos estao corretos");
+    res.redirect("/cadastro/cliente");
   }
 }
 
-function carregarPaginaCadastroCliente(req, res, next) {  // Get
+function carregarPaginaCadastroCliente(req, res, next) {
   res.render("cadastroClienteView");
 }
 
@@ -27,10 +25,14 @@ async function carregarPaginaListarCliente(req, res, next) {
   const nome = req.query.pesquisarCliente;
 
   if (nome) {
-    const listaClientesNome = await clienteDao.selectAllNomeCliente(nome);
-    res.render("listarClientesView", {
-      lista: listaClientesNome,
-    });
+    try {
+      const listaClientesNome = await clienteDao.selectAllNomeCliente(nome);
+      res.render("listarClientesView", {
+        lista: listaClientesNome,
+      });
+    } catch (error) {
+      console.log("erro ao selecionar dados da tabela pelo nome");
+    }
   } else {
     try {
       const listaCliente = await clienteDao.selectAllCliente(); // consulta de todos os clientes do banco de dados
@@ -45,37 +47,39 @@ async function carregarPaginaListarCliente(req, res, next) {
 }
 
 function editarDadosCliente(req, res, next) {
+  
   const id = req.params.id;
   const clienteDao = new ClienteDAO();
   const cliente = new Cliente(req.body.nome, req.body.idade, req.body.email);
 
   clienteDao.updateClientesDB(cliente, id);
 
-  req.flash("sucesso",`Dados do ${cliente.nome} alterado com sucesso `)
-  res.redirect("/lista/cliente")
-
+  req.flash("sucesso", `Dados do ${cliente.nome} alterado com sucesso `);
+  res.redirect("/lista/cliente");
 }
 
-async function carregarPaginaEditarCliente(req, res, next) { // GET
+async function carregarPaginaEditarCliente(req, res, next) {
   const clienteDao = new ClienteDAO();
   const id = req.params.id;
-  const lista = await clienteDao.selectAllIdCliente(id);
-
-  res.render("editarClienteView", {
-    lista,
-  });
+  try {
+    const lista = await clienteDao.selectAllIdCliente(id);
+    res.render("editarClienteView", {
+      lista,
+    });
+  } catch (error) {
+    console.log("erro ao editar cliente");
+  }
 }
 
- function excluirCliente(req, res, next) {
+function excluirCliente(req, res, next) {
   const id = req.params.id;
   const clienteDao = new ClienteDAO();
 
   clienteDao.excluirClienteDB(id);
 
   // const listaCliente = await clienteDao.selectAllCliente()
-  req.flash("sucesso",`Dados excluido com sucesso`)
-  res.redirect("/lista/cliente")
-
+  req.flash("sucesso", `Dados excluido com sucesso`);
+  res.redirect("/lista/cliente");
 }
 
 module.exports = {
